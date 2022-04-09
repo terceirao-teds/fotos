0f87858df6e723826d4bad963274f96204296dce
Yasushiki <66795854+Yasushiki@users.noreply.github.com>
2022-04-09 19:25:50 +0000

    tudo

---
+++ html.js
@@ -0,0 +1,40 @@
+var text = [
+  "&lt;!<span>DOCTYPE</span> <span3>html</span3>&gt;",
+  "&lt;html&gt;",
+  "&nbsp;&lt;head&gt;",
+  "&nbsp;&lt;/head&gt;",
+  
+  "&nbsp;&lt;body&gt;",
+
+  "dasda",
+  
+  "&nbsp;&nbsp;<span>Pelo</span> <span2>visto você</span2> <span3>escaneou nosso</span3> <span>QR Code.</span>",
+  "&nbsp;&nbsp;<span>Bom...</span> <span2>já que</span2> <span3>você chegou</span3> <span4>até </span4><span>aqui,</span>",
+  "&nbsp;&nbsp;<span>gostaria</span> <span2>de seguir</span2> <span3>para o</span3> <span>próximo passo?</span>",
+  "",
+  "&nbsp;&nbsp;<span>Se sim,</span> <span2>cique</span2> <a href=''>aqui</a><span3>, se não,</span3> <span4>apenas</span4> <span>saia da página.</span>",
+  
+  "&nbsp;&lt;/body&gt;",
+  
+  "&lt;html&gt;",
+  ""
+];
+
+var table = document.getElementsByTagName("table")[0];
+
+for(let i = 0; i < text.length; i++) {
+  var tr = document.createElement("tr");
+
+  var tdN = document.createElement("td");
+  tdN.innerText = i+1;
+
+  var tdH = document.createElement("td");
+  tdH.innerHTML = text[i];
+  
+  tr.appendChild(tdN);
+  tr.appendChild(tdH);
+
+  table.appendChild(tr);
+  
+}
+
